75c9d77684b1decd24a2b1cf61ccdc6b8abb20bc
r3drighthand <ryanmccool13@gmail.com>
2017-03-17 11:33:23 -0500

    Changed hash name, tested more driver code, reformatted pseudo

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,53 +1,54 @@
-# Write a program that will allow users to enter data
-# Enter the name of the document at the top
-# Ask the user to input informatiom
-# Create an empty hash
-# Prompt and allow an interior designer to enter the details of a given client: the client's 
-# name, age, number of children, decor theme
-# save each detail into created hash
-# Convert any user input to the appropriate data type.
-# Print the hash back out to the screen when the designer has answered all of the questions.
-# Give the user the opportunity to update a key, if "none", skip it. 
-# If the user enters name of what they'd like to change, allow them to input the correction
-# Overwrite the existing value pair  
-# Print the latest version of the hash, and exit the program.
+=begin 
+# PSEUDOCODE
+Write interface program so user can enter data.
+User should receive prompts for each data entry.
+Convert user input into correct data type.
+Store that data in variables.
+Those variables should be the values of a key/value pair in an empty hash.
+Add client data to empty hash with each entry.
+Print hash to console.
+Prompt user to correct any incorrect info, or type 'done'.
+Set up one-time loop to store user input, or exit program if user types 'done'.
+Print updated hash.
+
+=end 
 
 puts "Interior Designer Job Application"
-puts "Please enter the following:"
+puts "Please provide the following information:"
 
-client_information = {}
+client_data = {}
 
 puts "Full name:"
 full_name = gets.chomp
-client_information["full_name"] = full_name
+client_data["full_name"] = full_name
 
 
 puts "Age:"
 age = gets.chomp.to_i 
-client_information["age"] = age
+client_data["age"] = age
  
  
 puts "Number of children:"
-number_of_children = gets.chomp.to_i 
-client_information["number_of_children"] = number_of_children
+children = gets.chomp.to_i 
+client_data["children"] = children
  
  
 puts "Decor theme:"
 decor_theme = gets.chomp
-client_information["decor_theme"] = decor_theme
+client_data["decor_theme"] = decor_theme
 
 puts " "
-puts client_information
+puts client_data
 
 "Name: #{full_name}"
 "Age: #{age}"
-"Number of children: #{number_of_children}"
+"Number of children: #{children}"
 "Decor theme: #{decor_theme}"
 
 puts ""
 puts "Are there any changes you would like to make to the information 
-entered above? Please specify where by typing: 'name', 'age', 'number o
-f children', 'decor theme', or 'none' if there are no changes."
+entered above? Please specify where by typing: 'name', 'age', 'children', 
+'decor', or 'none' if there are no changes."
 
 
 edit_info = gets.chomp
@@ -55,16 +56,16 @@ edit_info = gets.chomp
         puts "Thank you."
     elsif "name"
         puts "Please enter the correct information:"
-        client_information[edit_info] = gets.chomp
+        client_data[edit_info] = gets.chomp
     elsif "age"
         puts "Please enter the correct information:"
-        client_information[edit_info] = gets.chomp
+        client_data[edit_info] = gets.chomp
     elsif "number of children"
         puts "Please enter the correct information:"
-        client_information[edit_info] = gets.chomp
+        client_data[edit_info] = gets.chomp
     else "decor theme"
         puts "Please enter the correct information:"
-        client_information[edit_info] = gets.chomp
+        client_data[edit_info] = gets.chomp
     end 
 
 puts " "
