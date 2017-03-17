f39b99e8bef37c47d94cdaa50abfe78c2a69b204
r3drighthand <ryanmccool13@gmail.com>
2017-03-16 21:17:15 -0500

    Added method solutions from ruby docs for each question, including multiple solutions where applicable

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,40 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-p "iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-p "zom".insert(1, 'o')
+puts "zom".insert(1, 'o')
+puts "zom".gsub('o', 'oo')
 # => “zoom”
 
-p "enhance".center(15)
+puts "enhance".center(15)
 # => "    enhance    "
 
-p "Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-p "the usual".insert(-1, ' suspects')
+puts "the usual".insert(9, ' suspects')
 #=> "the usual suspects"
 
-p " suspects".insert(0, 'the usual')
+puts " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-p"The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-p "The mystery of the missing first letter".delete "T"
+puts "The mystery of the missing first letter".delete "T"
+puts "The mystery of the missing first letter".sub("T", "")
 # => "he mystery of the missing first letter"
 
-p "Elementary,    my   dear        Watson!".center(10)
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
+puts "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-p "z".ord
+puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# It's ASCII code.
 
-p "How many times does the letter 'a' appear in this string?".count "a"
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
