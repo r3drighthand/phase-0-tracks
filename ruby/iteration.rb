8f41bf3fa38bbdc1e13f5198b8680ae2bcc02372
r3drighthand <ryanmccool13@gmail.com>
2017-03-17 18:21:58 -0500

    Added method that takes a block, interpolates parameters, and prints

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,1 +1,8 @@
-# ITERATION
+# RELEASE 1
+
+def week_days
+	puts "Today is Friday!"
+	yield("Thursday", "Wednesday", "Tuesday")
+end
+
+week_days { |day1, day2, day3| puts "The last three days were: #{day1}, #{day2}, and #{day3}!" }
