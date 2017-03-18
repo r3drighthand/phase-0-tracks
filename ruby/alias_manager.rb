bc380374b7adb4787b49d43f5ee7e385c0158154
r3drighthand <ryanmccool13@gmail.com>
2017-03-18 14:31:01 -0500

    Wrote code that calls nested methods on first name and last name, stores output in new variables, wrote code that adds last name to first name with space in between, and capitalizes output.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,5 +1,5 @@
 
-# LOGIC
+# PSEUDO
 
 # 1: Swapping the first and last name.
 # 2: Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
@@ -7,18 +7,10 @@
 # consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', 
 # and 'd' would become 'f'.
 
-# Write 3 methods:
-	# Method that swaps first, last name
-	# Method that shifts vowels
-	# Method that shifts consonants 
-# Join string back together
 
 =begin 
-This is the most complex algorithm you've had to write, and the toughest release 
-of this challenge. How will you break it down into manageable pieces? Try to 
-identify all of the mini-challenges and considerations that show up in your algorithm, 
-such as:
-	When will it be helpful to convert the string to an array to work with it more easily?
+
+When will it be helpful to convert the string to an array to work with it more easily?
 		when rotating vowels, consonants => iterating through arrays to match & rotate 
 			chars, then joining arrays back to strings.
 	How will you figure out whether a letter is a vowel?
@@ -35,41 +27,22 @@ such as:
 		reassigning values (if 'z', 'z' == 'a')
 =end 
 
-def name_swap(name)
-	name.reverse.split(' ')
-end 
 
-name_swap("Felicia Torres")
+###################
 
 
-
-def vowel_shift(str) # define method that takes a string as its argument
-    vowels = ["a", "e", "i", "o", "u"]
-    str = str.split('')
-    new_str = str.map do |char| 
-        if vowels.include?(char)
-            vowels.rotate(1)[vowels.index(char)] # <= TEST 
-        else 
-            char
-        end 
-    end 
-    new_str.join
-end 
-
-# p vowel_shift("Felicia Torres")
-
-puts "Welcome to the Secret Agent Alias Manager!"
+puts "Welcome to the Alias Manager!"
 puts "What is your first name?"
 first_name = gets.chomp.downcase
     
-puts "What's your last name?"
+puts "What is your last name?"
 last_name = gets.chomp.downcase
 
-# split vowels of names, same for consonants. Join them back together
-# write 2 methods: one for vowels, one for consonants. 
-# set up loop to iterate through characters
-# Rotate, not next
 
+# def name_swap(str)
+#    str = str.split(' ').reverse
+#    str.join
+# end 
 
 
 def vowel_shift(str)
@@ -86,7 +59,8 @@ def vowel_shift(str)
 end
 
 def consonant_shift(str)
-  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+  consonants = ["b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", 
+  	"p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
   str = str.split('')
   str_new = str.map do |char|
     if consonants.include?(char)
@@ -97,3 +71,12 @@ def consonant_shift(str)
   end
   str_new.join
 end
+
+# Store methods in variables
+# Combine at end of UI
+
+
+new_first = consonant_shift(vowel_shift(first_name)).capitalize
+new_last = consonant_shift(vowel_shift(last_name)).capitalize
+
+p new_last + " " + new_first
