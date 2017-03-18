99ed6e7abad3060b8da2a320b427f28ba5690dab
r3drighthand <ryanmccool13@gmail.com>
2017-03-17 20:52:48 -0500

    Created hash containing several nested hashes and arrays, divided into 5 organ systems of human biology

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,86 @@
-# NDS
+
+bio_array = {
+  circulatory: {
+    heart: {
+      chambers: [
+        "r_ventricle", 
+        "l_ventricle", 
+        "r_atrium", 
+        "l_atrium"
+        ]
+      },
+    blood_cells: [
+      "red", 
+      "white"
+      ],
+    arteries: [
+      "carotid", 
+      "brachial", 
+      "subclavian"
+      ]
+    },
+  digestive: {
+    stomach: [
+      "body", 
+      "duodenum", 
+      "cardia", 
+      "fundus"
+      ],
+    intestines: [
+      "ascending", 
+      "transverse", 
+      "descending"]
+    },
+  nervous: {
+    brain: [
+      "parietal", 
+      "frontal", 
+      "temporal", 
+      "occipital", 
+      "cerebellum"
+      ],
+    nerve: [
+      "axon_terminal", 
+      "myelin_sheath", 
+      "synapse", 
+      "dendrite", 
+      "axon"
+      ]
+    },
+  respiratory: {
+    lungs: [
+      "pleura", 
+      "alveoli", 
+      "r_bronchi", 
+      "l_bronchi"
+      ],
+    larynx: [
+      "hyoid", 
+      "trachea", 
+      "epiglottis", 
+      "thyroid", 
+      "parathyroid"
+      ]
+  },
+  skeletal: {
+    bone_terms: [
+      "epiphysis", 
+      "diaphysis"
+      ],
+    bone_list: [
+      "skull", 
+      "femur", 
+      "carpals", 
+      "clavicle", 
+      "sternum", 
+      "ribs", 
+      "radius"
+      ],
+    cartilage: [
+      "elastic", 
+      "hyaline", 
+      "fibrous"
+      ]
+  }
+}
+  
