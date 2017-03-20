9b0f9b38e48223ab713c0142291ab7bd0f94dc1b
r3drighthand <ryanmccool13@gmail.com>
2017-03-20 14:45:00 -0500

    Added 5 methods to Puppy class, driver code works

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,10 +2,32 @@
 
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	# fetch toy method
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
+
+	# speak method
+	def speak(x)
+		puts "Woof! " * x
+	end 
+
+	# roll over method
+	def roll_over
+		puts "*rolls over*"
+	end 
+
+	# dog year conversion method
+	def dog_years(x)
+		dog_age = x * 7
+		puts "I am (secretly) #{dog_age} years old."
+	end	
+
+	# attack method
+	def attack
+		puts "Grrr!"
+	end
 
 end
 
@@ -16,3 +38,7 @@ end
 # initialize instance of Puppy
 ruthie = Puppy.new
 ruthie.fetch("ball")
+ruthie.speak(5)
+ruthie.roll_over
+ruthie.dog_years(12)
+ruthie.attack
