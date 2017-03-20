c2baec91b79d6ffa73b2326022534dba363a5af6
r3drighthand <ryanmccool13@gmail.com>
2017-03-20 13:21:37 -0500

    Outlined puppy class with characteristics and behavior in comments

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,5 +1,27 @@
-# Class design for Puppy
+=begin
 
-class Puppy
+Puppy Class Design
 
-	
+SPECIES ------------------------
+Canis puppus
+
+CHARACTERISTICS ----------------
+Name: varies
+Friendly: yes
+Trained: no
+Fur color: varies
+Size: varies
+Breed: varies
+
+BEHAVIOR -----------------------
+Bark
+Run
+Sit
+Swim
+Fetch
+Chase
+Dig
+Bite
+Growl
+
+=end 
