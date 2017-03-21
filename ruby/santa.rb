b45d327f8e6464f2d0e06cec76bd43104dca47ec
r3drighthand <ryanmccool13@gmail.com>
2017-03-21 10:04:11 -0500

    Created santa class, wrote 3 methods, tests clear

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,1 +1,26 @@
 # SANTA
+
+class Santa
+
+
+
+	def initialize
+		puts "Initializing Santa sequence..."
+		#attr gender = string
+		#attr ethnicity = string
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end 
+
+end
+
+# DRIVER CODE
+santa = Santa.new
+santa.speak
+santa.eat_milk_and_cookies("liver cookie")
