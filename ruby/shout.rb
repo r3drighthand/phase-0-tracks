068a4c0a81ce84001ea15c6188d6863b57eabd3b
r3drighthand <ryanmccool13@gmail.com>
2017-03-22 13:36:31 -0500

    Commented out old code for release 1, wrote new shout module and 2 classes that include shout, ran driver code, tests work

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -18,3 +18,26 @@ p Shout.yelling_happily("Come back")
 
 =end 
 
+module Shout
+	def yell_angrily(words)
+		puts words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		puts words + "!!!" + " :)"
+	end
+end
+
+class SamJackson
+	include Shout
+end
+
+class BruceWillis
+	include Shout
+end
+
+unbreakable = SamJackson.new
+unbreakable.yell_angrily("I'm not a mistake")
+
+die_hard = BruceWillis.new
+die_hard.yell_happily("Yippee ki yay, mother****er")
