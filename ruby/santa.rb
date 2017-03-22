3a65ea7e10d88e1fefc3f97f03aec9f729e4692c
r3drighthand <ryanmccool13@gmail.com>
2017-03-22 13:20:37 -0500

    Refactored/updated code to iterate 50 times and add new class instances to santarray, passing arguments of gender/ethnicity, added getter and setters, wrote code to move bad reindeer to end of array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,32 +1,67 @@
-# SANTA
+class Santa 
+    
+    attr_reader :ethnicity
+    attr_accessor :age, :gender, :reindeer_ranking
+    
+    def initialize(gender, ethnicity)
+        puts "Initializing santa sequence ..."
+        @gender = gender
+        @ethnicity = ethnicity
+        @age = 0 
+        @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    end
+    
+    def eats_cookie(cookie)
+        puts "That was a good #{cookie} cookie!"
+    end 
+    
+    def speak
+        puts "Ho ho ho! Haaaaappy holidays!"
+    end 
+    
+    def celebrate_birthday
+        @age += 1 
+    end 
+    
+    def get_mad_at(bad_reindeer)
+        @reindeer_ranking.delete(bad_reindeer)
+        @reindeer_ranking.push(bad_reindeer) # changed from << bad_reindeer
+    end 
+    
+    def gender_reassignment=(new_gender)
+        @gender = new_gender
+    end 
+    
+end 
 
-class Santa
 
-attr_accessor :gender, :ethnicity
+#DRIVER CODE
 
-	def initialize(gender, ethnicity)
-		puts "Initializing Santa sequence..."
-		@gender = gender
-		@ethnicity = ethnicity
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
-			"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
-	end
+# caitlin = Santa.new("female", "white")
+# caitlin.eats_cookie("snickerdoodle")
+# caitlin.speak
+# p caitlin.celebrate_birthday
+# p caitlin.get_mad_at("Blitzen")
+# p caitlin.get_mad_at("Dasher")
 
-	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
-	end 
 
-	def eat_cookies(cookie)
-		puts "That was a good #{cookie} cookie!"
-	end 
+santarray = []
+gender = ["female", "male", "nonbinary", "gender neutral", "trans male", "trans female", "gender fluid"]
+ethnicity = ["Chinese", "Pacific Islander", "Welsh", "French", "Russian", "Egyptian", "Brazilian"]
+# gender.length.times do |i|
+#    santarray << Santa.new(gender[i], ethnicity[i])
+# end 
 
+50.times do |i|
+	santarray << Santa.new(gender[i], ethnicity[i])
 end
 
-# DRIVER CODE
-# santa = Santa.new
-# santa.speak
-# santa.eat_milk_and_cookies("cononut")
+# Making sure 50 santas were added to array
+# p santarray.length
+
+p santarray
+
+
 
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", 
