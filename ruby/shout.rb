74ef62a1d78e611a64e8ab35f6c3321a98b9f5a2
r3drighthand <ryanmccool13@gmail.com>
2017-03-22 13:32:43 -0500

    Added module shout with 2 methods, driver code works

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,1 +1,20 @@
-# MIXINS AND FIXINS
+
+=begin 
+
+module Shout
+ 	def self.yell_angrily(words)
+ 		words + "!!!" + " :("
+    end
+
+    def self.yell_happily(words)
+ 		words + "!!!" + " :)"
+    end
+end
+
+
+# DRIVER CODE
+p Shout.yell_angrily("Go away")
+p Shout.yelling_happily("Come back")
+
+=end 
+
