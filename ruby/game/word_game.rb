bb0d8d41344d2563e29f765dce31d260e4cb93ad
r3drighthand <ryanmccool13@gmail.com>
2017-03-25 22:17:59 -0500

    Working on game design

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -28,23 +28,177 @@
 # Write method for user to enter a word.
 # Write method to keep track of word guesses.
 # Write method to determine whether player has won or lost, and print messages.
+# Method to count how long a word is
+
+# Take a word and turn it into dashes
+# How? An array?
+
 
 class WordGame
 
+	attr_reader :guess_count
 	# word_length, guess_count, guess_remaining, game_over, mystery_word, 
 	# letters_guessed, word_array
 
-	def initialize
-		@word_length = word.length
-    	@guess_count = 0
-    	@guess_remaining = word.length + 10
-    	@game_over = false
-    	@mystery_word = mystery_word
-    	@letters_guessed = []
-    	@word_array = @word.split(' ')
+	def initialize(word)
+		@word = word 
 	end
 
-	def 
+	def letters_guessed
+		letter_bank = []
+		letter_bank << letter_guessed 
+		p letter_bank
+	end 
+
+
+end 
+
+# DRIVER CODE
+game = WordGame.new("unicorn")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# bumblebees game class
+
+# class WordGame
+
+#   attr_accessor :guess_count :word, :letters_guessed, :word_input, :word_array, :secret_word, :guess_remaining
+
+#   def initialize(word)
+#     @word_length = word.length
+#     @guess_count = 0
+#     @guess_remaining = word.length + 10
+#     @game_over = false
+#     @secret_word = secret_word
+#     @letters_guessed = []
+#     @word_array = @word.split(' ')
+#   end 
+
+#   def letter_bank(letter)
+#     letters_guessed << letter
+#   end
+
+#   def letter=(letter)
+#     @letter = letter
+
+#   def guessing_letters (word)
+#     @word_array = @word.split(' ')
+#   end
+
+#   def update_spaces(letter)
+#     @word_array.each do |letter|
+#       if @word_array.include? letter
+#         print @letter
+#         else
+#           @word_array.each print '_ '
+#       end
+#   end
+
+#   def create_spaces(word)
+#     @letter_spaces = word.length
+#     @secret_word = letter_spaces.map {|word| '_ '}
+#   end
+
+#   def start_game 
+#     until @guess_limit == @guess_count 
+#       @guess_count =+ 1
+#     if @solution.include?(@letter)
+#       puts "good!"
+#     else 
+#       puts "bad..."
+#     end 
+
+#   def game_over
+#     p @letter_spaces
+#     if !@letter_spaces.include?('_ ')
+#       @game_over = true
+#       p @game_over
+#     else
+#       @game_over = false
+#       p @game_over
+#     end
+#   end 
+
+# end
+
+
+# # User Interface
+
+# puts "Ready Player 1! Enter a word for Player 2 to guess:"
+# game = WordGame.new(word)
+# game.word = gets.chomp
+
+# puts "Saving word... Word is now hidden!"
+
+# game.start_game(game.word)
+# game.word
+
+# game.create_spaces(game.word)
+
+# puts "Ready Player 2! Guess the secret word, one letter at a time!"
+# puts "You have #{game.guess_remaining} guesses remaining. Go!"
+
+# @letter = gets.chomp
+# game.update_spaces(@letter)
+# game.letter_bank(@letter)
+# p @letters_guessed
+
+
+
 
 
 
--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -1,1 +1,15 @@
-# DESIGN TEST 2
+require_relative 'word_game'
+
+describe WordGame do
+	let(:wordgame) {WordGame.new("---")}
+
+	it "test description" do
+		expect(_). to eq "---"
+	end 
+
+	it "test description" do
+		# genie.grant_wish("pony")
+		# genie.grant_wish("alligator")
+		# expect(genie.favorite_wish).to eq "alligator"
+	end
+end
