b0e18aba2ffda6d72c104477f286dbc542e49c77
r3drighthand <ryanmccool13@gmail.com>
2017-03-26 14:11:19 -0500

    Deleted comments

--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -4,6 +4,6 @@ describe WordGame do
 	let(:game) {WordGame.new("unicorn")}
 
 	it "turns letters into spaces" do
-		expect(game.create_spaces). to eq "_ _ _ _ _ _ _ "
+		expect(game.create_spaces). to eq "_______"
 	end 
 end 
