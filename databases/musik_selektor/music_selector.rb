e2e7245a13f74afb84c22a6513b3974a35ef8193
r3drighthand <ryanmccool13@gmail.com>
2017-04-09 14:20:22 -0500

    Added nested data structures

--- databases/musik_selektor/music_selector.rb
+++ databases/musik_selektor/music_selector.rb
@@ -8,13 +8,29 @@ require 'sqlite3'
 db = SQLite3::Database.new("music.db")
 # db.results_as_hash = true
 
-happy = []
+happy = [ {"Singur"=>"Oskar Schuster"}, {"Debut"=>"Melanie Laurent"}, 
+	{"II"=>"Jean-Michel Blais"}, {"Nightingale"=>"Lo Mimieux"}, 
+	{"Hers"=>"The Arcadian Wild"}, {"Gizeh"=>"Oskar Schuster"}, {"The Blooming"=>"Einstock"}, 
+	{"Almost Idyllic"=>"Sleeping At Last"}, {"I Promise"=>"Alex Kozobolis"}, 
+	{"Tokka"=>"Agnes Obel"}, {"Sunday Stroll"=>"Benny Treskow"} ]
 
-sad = []
+sad = [ {"The Path to Highgate"=>"Chad Lawson"}, {"Pensiero Notturno"=>"Fabrizio Paterlini"}, 
+	{"Wandering"=>"Dirk Maassen"}, {"A Web of Lies"=>"August Wilhelmsson"}, 
+	{"All the Stars Are Out Tonight"=>"Danny Norbury"}, {"A Magnificent Gray"=>"Slow Meadow"}, 
+	{"Vladimir's Blues"=>"Max Richter"}, {"Echoes of You"=>"Oneke"}, 
+	{"Chord Left"=>"Agnes Obel"}, {"Inevitable Things"=>"Dmitry Evgrafov"} ]
 
-determined = []
+determined = [ {"The Aviators"=>"Helen Jane Long"}, {"La Mer"=>"Dirk Maassen"}, 
+	{"183 Times"=>"Greg Haines"}, {"Augenblick"=>"Dirk Maassen"}, {"Airam"=>"Nil Ciuro"}, 
+	{"The Approaching Night"=>"Philip Wesley"}, {"Phase One"=>"Gabriel Parker"}, 
+	{"Claroscuro"=>"Bruno Sanfilippo"}, {"Delicate Transitions"=>"Gavin Luke"}, 
+	{"Explosions in a Four-Chambered Heart"=>"Moon Ate the Dark"} ]
 
-calm = []
+calm = [ {"Norwegian Beauty"=>"Evelyn Stein"}, {"Sugarcane"=>"Ana Olgica"}, 
+	{"The Last Hour of Loneliness"=>"Elba"}, {"The Light She Brings"=>"Joep Beving"}, 
+	{"Synesthesia"=>"Peter Sandberg"}, {"Romances"=>"Amity Cadet"}, 
+	{"Alpine Sketch"=>"Christian Loffler"}, {"Travelling"=>"James Spiteri"}, 
+	{"In The Mist"=>"Harry Rosenpal"}, {"Ascend"=>"Shaun Taylor McManus"} ]
 
 
 
