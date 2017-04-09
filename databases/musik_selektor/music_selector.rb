3a12b4a4455cf16745914f6ef9bd0e539839da4b
r3drighthand <ryanmccool13@gmail.com>
2017-04-08 23:58:05 -0500

    Pseudocoded next portions

--- databases/musik_selektor/music_selector.rb
+++ databases/musik_selektor/music_selector.rb
@@ -6,9 +6,20 @@
 
 require 'sqlite3'
 db = SQLite3::Database.new("musicology.db")
-db.results_as_hash = true
+# db.results_as_hash = true
+
+
 
 create_table_cmd  = <<-SQL
 	CREATE TABLE IF NOT EXISTS musicology(
 		id INTEGER PRIMARY KEY,
-		) 
+		song VARCHAR(225),
+		composer VARCHAR(225)
+	)
+SQL 
+
+# Be able to add song/composer key/value pairs?
+# Ordered according to mood?
+# Alternatively, use arrays/hashes outside of db; use db 
+# to log user's mood, data of entry, and weather conditions.
+# Could use .sample for external array... what is hash equivalent?
