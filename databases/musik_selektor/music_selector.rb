82b4b9be50f3f4095655e657756bd9e53a625b37
r3drighthand <ryanmccool13@gmail.com>
2017-04-09 20:29:22 -0500

    Fixed syntax errors

--- databases/musik_selektor/music_selector.rb
+++ databases/musik_selektor/music_selector.rb
@@ -5,49 +5,52 @@
 # and their respective composers. 
 
 require 'sqlite3'
+
 db = SQLite3::Database.new("music.db")
-# db.results_as_hash = true
+# db.results_as_hash = true 
 
-happy = [ {"Singur"=>"Oskar Schuster"}, {"Debut"=>"Melanie Laurent"}, 
-	{"II"=>"Jean-Michel Blais"}, {"Nightingale"=>"Lo Mimieux"}, 
-	{"Hers"=>"The Arcadian Wild"}, {"Gizeh"=>"Oskar Schuster"}, {"The Blooming"=>"Einstock"}, 
-	{"Almost Idyllic"=>"Sleeping At Last"}, {"I Promise"=>"Alex Kozobolis"}, 
-	{"Tokka"=>"Agnes Obel"}, {"Sunday Stroll"=>"Benny Treskow"} ]
-
-sad = [ {"The Path to Highgate"=>"Chad Lawson"}, {"Pensiero Notturno"=>"Fabrizio Paterlini"}, 
-	{"Wandering"=>"Dirk Maassen"}, {"A Web of Lies"=>"August Wilhelmsson"}, 
-	{"All the Stars Are Out Tonight"=>"Danny Norbury"}, {"A Magnificent Gray"=>"Slow Meadow"}, 
-	{"Vladimir's Blues"=>"Max Richter"}, {"Echoes of You"=>"Oneke"}, 
-	{"Chord Left"=>"Agnes Obel"}, {"Inevitable Things"=>"Dmitry Evgrafov"} ]
-
-determined = [ {"The Aviators"=>"Helen Jane Long"}, {"La Mer"=>"Dirk Maassen"}, 
-	{"183 Times"=>"Greg Haines"}, {"Augenblick"=>"Dirk Maassen"}, {"Airam"=>"Nil Ciuro"}, 
-	{"The Approaching Night"=>"Philip Wesley"}, {"Phase One"=>"Gabriel Parker"}, 
-	{"Claroscuro"=>"Bruno Sanfilippo"}, {"Delicate Transitions"=>"Gavin Luke"}, 
-	{"Explosions in a Four-Chambered Heart"=>"Moon Ate the Dark"} ]
-
-calm = [ {"Norwegian Beauty"=>"Evelyn Stein"}, {"Sugarcane"=>"Ana Olgica"}, 
-	{"The Last Hour of Loneliness"=>"Elba"}, {"The Light She Brings"=>"Joep Beving"}, 
-	{"Synesthesia"=>"Peter Sandberg"}, {"Romances"=>"Amity Cadet"}, 
-	{"Alpine Sketch"=>"Christian Loffler"}, {"Travelling"=>"James Spiteri"}, 
-	{"In The Mist"=>"Harry Rosenpal"}, {"Ascend"=>"Shaun Taylor McManus"} ]
-
-conflicted = [ {"Worried Sky"=>"Jean Petri"}, {"Partly Cloudy"=>"Silver Maple"}, 
-	{"Unstitch"=>"Anna Rose Carter"}, {"Anthem"=>"Dmitry Evgrafov"}, {"Norrsken"=>"Karin Borg"}, 
-	{"Islands"=>"Chad Lawson"}, {"Denouement"=>"Lo Mimieux"}, 
-	{"Nocturnal Waltz"=>"Johannes Bornlof"}, {"Remove the Complexities"=>"Peter Sandberg"}, 
-	{"Ab Ovo"=>"Joep Beving"} ]
-
-
-create_table_cmd  = <<-SQL
-	CREATE TABLE IF NOT EXISTS music(
-		id INTEGER PRIMARY KEY,
-		date VARCHAR(225),
-		mood VARCHAR(225)
-	)
-SQL 
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS kittens(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
 
 db.execute(create_table_cmd)
+
+# happy = [ {"Singur"=>"Oskar Schuster"}, {"Debut"=>"Melanie Laurent"}, 
+# 	{"II"=>"Jean-Michel Blais"}, {"Nightingale"=>"Lo Mimieux"}, 
+# 	{"Hers"=>"The Arcadian Wild"}, {"Gizeh"=>"Oskar Schuster"}, {"The Blooming"=>"Einstock"}, 
+# 	{"Almost Idyllic"=>"Sleeping At Last"}, {"I Promise"=>"Alex Kozobolis"}, 
+# 	{"Tokka"=>"Agnes Obel"}, {"Sunday Stroll"=>"Benny Treskow"} ]
+
+# sad = [ {"The Path to Highgate"=>"Chad Lawson"}, {"Pensiero Notturno"=>"Fabrizio Paterlini"}, 
+# 	{"Wandering"=>"Dirk Maassen"}, {"A Web of Lies"=>"August Wilhelmsson"}, 
+# 	{"All the Stars Are Out Tonight"=>"Danny Norbury"}, {"A Magnificent Gray"=>"Slow Meadow"}, 
+# 	{"Vladimir's Blues"=>"Max Richter"}, {"Echoes of You"=>"Oneke"}, 
+# 	{"Chord Left"=>"Agnes Obel"}, {"Inevitable Things"=>"Dmitry Evgrafov"} ]
+
+# determined = [ {"The Aviators"=>"Helen Jane Long"}, {"La Mer"=>"Dirk Maassen"}, 
+# 	{"183 Times"=>"Greg Haines"}, {"Augenblick"=>"Dirk Maassen"}, {"Airam"=>"Nil Ciuro"}, 
+# 	{"The Approaching Night"=>"Philip Wesley"}, {"Phase One"=>"Gabriel Parker"}, 
+# 	{"Claroscuro"=>"Bruno Sanfilippo"}, {"Delicate Transitions"=>"Gavin Luke"}, 
+# 	{"Explosions in a Four-Chambered Heart"=>"Moon Ate the Dark"} ]
+
+# calm = [ {"Norwegian Beauty"=>"Evelyn Stein"}, {"Sugarcane"=>"Ana Olgica"}, 
+# 	{"The Last Hour of Loneliness"=>"Elba"}, {"The Light She Brings"=>"Joep Beving"}, 
+# 	{"Synesthesia"=>"Peter Sandberg"}, {"Romances"=>"Amity Cadet"}, 
+# 	{"Alpine Sketch"=>"Christian Loffler"}, {"Travelling"=>"James Spiteri"}, 
+# 	{"In The Mist"=>"Harry Rosenpal"}, {"Ascend"=>"Shaun Taylor McManus"} ]
+
+# conflicted = [ {"Worried Sky"=>"Jean Petri"}, {"Partly Cloudy"=>"Silver Maple"}, 
+# 	{"Unstitch"=>"Anna Rose Carter"}, {"Anthem"=>"Dmitry Evgrafov"}, {"Norrsken"=>"Karin Borg"}, 
+# 	{"Islands"=>"Chad Lawson"}, {"Denouement"=>"Lo Mimieux"}, 
+# 	{"Nocturnal Waltz"=>"Johannes Bornlof"}, {"Remove the Complexities"=>"Peter Sandberg"}, 
+# 	{"Ab Ovo"=>"Joep Beving"} ]
+ 
+
+
 
 # Be able to add song/composer key/value pairs?
 # Ordered according to mood?
@@ -59,8 +62,8 @@ db.execute(create_table_cmd)
 # 	db.execute("INSERT INTO music (date, mood) VALUES (?, ?)", ('#{date}', '#{mood}');")
 # end 
 
-# puts "Enter today's date (mm/dd/yy):"
-# date = gets.chomp
+# puts "What is your name?"
+# name = gets.chomp
 
 # puts "How do you feel? We'll match your mood with music!" 
 # puts "Please type one of the following: 'happy', 'sad', 'determined', 'calm', or 'conflicted'."
