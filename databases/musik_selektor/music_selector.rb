cc2c7c4ac2e80abec50423e0f48a773eafd6e3d9
r3drighthand <ryanmccool13@gmail.com>
2017-04-09 20:50:16 -0500

    Can add user input data to music db, working on ORM

--- databases/musik_selektor/music_selector.rb
+++ databases/musik_selektor/music_selector.rb
@@ -20,35 +20,35 @@ SQL
 
 db.execute(create_table_cmd)
 
-# happy = [ {"Singur"=>"Oskar Schuster"}, {"Debut"=>"Melanie Laurent"}, 
-# 	{"II"=>"Jean-Michel Blais"}, {"Nightingale"=>"Lo Mimieux"}, 
-# 	{"Hers"=>"The Arcadian Wild"}, {"Gizeh"=>"Oskar Schuster"}, {"The Blooming"=>"Einstock"}, 
-# 	{"Almost Idyllic"=>"Sleeping At Last"}, {"I Promise"=>"Alex Kozobolis"}, 
-# 	{"Tokka"=>"Agnes Obel"}, {"Sunday Stroll"=>"Benny Treskow"} ]
+happy = [ {"Singur"=>"Oskar Schuster"}, {"Debut"=>"Melanie Laurent"}, 
+	{"II"=>"Jean-Michel Blais"}, {"Nightingale"=>"Lo Mimieux"}, 
+	{"Hers"=>"The Arcadian Wild"}, {"Gizeh"=>"Oskar Schuster"}, {"The Blooming"=>"Einstock"}, 
+	{"Almost Idyllic"=>"Sleeping At Last"}, {"I Promise"=>"Alex Kozobolis"}, 
+	{"Tokka"=>"Agnes Obel"}, {"Sunday Stroll"=>"Benny Treskow"} ]
 
-# sad = [ {"The Path to Highgate"=>"Chad Lawson"}, {"Pensiero Notturno"=>"Fabrizio Paterlini"}, 
-# 	{"Wandering"=>"Dirk Maassen"}, {"A Web of Lies"=>"August Wilhelmsson"}, 
-# 	{"All the Stars Are Out Tonight"=>"Danny Norbury"}, {"A Magnificent Gray"=>"Slow Meadow"}, 
-# 	{"Vladimir's Blues"=>"Max Richter"}, {"Echoes of You"=>"Oneke"}, 
-# 	{"Chord Left"=>"Agnes Obel"}, {"Inevitable Things"=>"Dmitry Evgrafov"} ]
+sad = [ {"The Path to Highgate"=>"Chad Lawson"}, {"Pensiero Notturno"=>"Fabrizio Paterlini"}, 
+	{"Wandering"=>"Dirk Maassen"}, {"A Web of Lies"=>"August Wilhelmsson"}, 
+	{"All the Stars Are Out Tonight"=>"Danny Norbury"}, {"A Magnificent Gray"=>"Slow Meadow"}, 
+	{"Vladimir's Blues"=>"Max Richter"}, {"Echoes of You"=>"Oneke"}, 
+	{"Chord Left"=>"Agnes Obel"}, {"Inevitable Things"=>"Dmitry Evgrafov"} ]
 
-# determined = [ {"The Aviators"=>"Helen Jane Long"}, {"La Mer"=>"Dirk Maassen"}, 
-# 	{"183 Times"=>"Greg Haines"}, {"Augenblick"=>"Dirk Maassen"}, {"Airam"=>"Nil Ciuro"}, 
-# 	{"The Approaching Night"=>"Philip Wesley"}, {"Phase One"=>"Gabriel Parker"}, 
-# 	{"Claroscuro"=>"Bruno Sanfilippo"}, {"Delicate Transitions"=>"Gavin Luke"}, 
-# 	{"Explosions in a Four-Chambered Heart"=>"Moon Ate the Dark"} ]
+determined = [ {"The Aviators"=>"Helen Jane Long"}, {"La Mer"=>"Dirk Maassen"}, 
+	{"183 Times"=>"Greg Haines"}, {"Augenblick"=>"Dirk Maassen"}, {"Airam"=>"Nil Ciuro"}, 
+	{"The Approaching Night"=>"Philip Wesley"}, {"Phase One"=>"Gabriel Parker"}, 
+	{"Claroscuro"=>"Bruno Sanfilippo"}, {"Delicate Transitions"=>"Gavin Luke"}, 
+	{"Explosions in a Four-Chambered Heart"=>"Moon Ate the Dark"} ]
 
-# calm = [ {"Norwegian Beauty"=>"Evelyn Stein"}, {"Sugarcane"=>"Ana Olgica"}, 
-# 	{"The Last Hour of Loneliness"=>"Elba"}, {"The Light She Brings"=>"Joep Beving"}, 
-# 	{"Synesthesia"=>"Peter Sandberg"}, {"Romances"=>"Amity Cadet"}, 
-# 	{"Alpine Sketch"=>"Christian Loffler"}, {"Travelling"=>"James Spiteri"}, 
-# 	{"In The Mist"=>"Harry Rosenpal"}, {"Ascend"=>"Shaun Taylor McManus"} ]
+calm = [ {"Norwegian Beauty"=>"Evelyn Stein"}, {"Sugarcane"=>"Ana Olgica"}, 
+	{"The Last Hour of Loneliness"=>"Elba"}, {"The Light She Brings"=>"Joep Beving"}, 
+	{"Synesthesia"=>"Peter Sandberg"}, {"Romances"=>"Amity Cadet"}, 
+	{"Alpine Sketch"=>"Christian Loffler"}, {"Travelling"=>"James Spiteri"}, 
+	{"In The Mist"=>"Harry Rosenpal"}, {"Ascend"=>"Shaun Taylor McManus"} ]
 
-# conflicted = [ {"Worried Sky"=>"Jean Petri"}, {"Partly Cloudy"=>"Silver Maple"}, 
-# 	{"Unstitch"=>"Anna Rose Carter"}, {"Anthem"=>"Dmitry Evgrafov"}, {"Norrsken"=>"Karin Borg"}, 
-# 	{"Islands"=>"Chad Lawson"}, {"Denouement"=>"Lo Mimieux"}, 
-# 	{"Nocturnal Waltz"=>"Johannes Bornlof"}, {"Remove the Complexities"=>"Peter Sandberg"}, 
-# 	{"Ab Ovo"=>"Joep Beving"} ]
+conflicted = [ {"Worried Sky"=>"Jean Petri"}, {"Partly Cloudy"=>"Silver Maple"}, 
+	{"Unstitch"=>"Anna Rose Carter"}, {"Anthem"=>"Dmitry Evgrafov"}, {"Norrsken"=>"Karin Borg"}, 
+	{"Islands"=>"Chad Lawson"}, {"Denouement"=>"Lo Mimieux"}, 
+	{"Nocturnal Waltz"=>"Johannes Bornlof"}, {"Remove the Complexities"=>"Peter Sandberg"}, 
+	{"Ab Ovo"=>"Joep Beving"} ]
  
 
 
@@ -63,46 +63,37 @@ def store_data(music, name, age, mood)
  	db.execute("INSERT INTO music (name, age, mood) VALUES (?, ?, ?)", [name, age, mood])
 end 
 
-# puts "What is your name?"
-# name = gets.chomp
+puts "What is your name?"
+name = gets.chomp
 
-# puts "How do you feel? We'll match your mood with music!" 
-# puts "Please type one of the following: 'happy', 'sad', 'determined', 'calm', or 'conflicted'."
-# mood = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
 
-# if mood == "happy"
-# 	p happy.sample
-# elsif mood == "sad"
-# 	p sad.sample
-# elsif mood == "determined"
-# 	p determined.sample
-# elsif mood == "calm"
-# 	p calm.sample
-# elsif mood == "conflicted"
-# 	p conflicted.sample
-# else 
-# 	puts "Please select one of the five listed moods... they're the only categories available."
-# end
+puts "How do you feel? We'll match your mood with music!" 
+puts "Please type one of the following: 'happy', 'sad', 'determined', 'calm', or 'conflicted'."
+mood = gets.chomp
 
-# store_data(music, data, mood)
+if mood == "happy"
+	p happy.sample
+elsif mood == "sad"
+	p sad.sample
+elsif mood == "determined"
+	p determined.sample
+elsif mood == "calm"
+	p calm.sample
+elsif mood == "conflicted"
+	p conflicted.sample
+else 
+	puts "Please select one of the five listed moods... they're the only categories available."
+end
 
-
-
-
-
+# store_data(music, name, age, mood)
 
 
 
 
-
-
-
-
-
-
- 
 # explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-# puts "#{kitten['name']} is #{kitten['age']}"
+# user_data = db.execute("SELECT * FROM music")
+# user_data.each do |user|
+	# puts "#{user['name']}, #{user['age'], feels #{user['mood']}"
 # end
