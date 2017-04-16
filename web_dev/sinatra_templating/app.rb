f34db58d241a7342de7474bfb673d014de2687ed
r3drighthand <ryanmccool13@gmail.com>
2017-04-16 11:46:14 -0500

    finished adding code from pps

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -24,4 +24,14 @@ post '/students' do
   redirect '/'
 end
 
+get '/students/campus' do
+  @campus = params[:campus]
+  @campus_students = db.execute("SELECT * FROM students WHERE campus=?",[params[:campus]])
+  erb :campus
+end
+
 # add static resources
+post '/campus' do
+  cam.execute("INSERT INTO campus (location) VALUES (?)", [params['location']])
+  redirect '/'
+end
